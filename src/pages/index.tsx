ed9dfa474ba3474b1b6966c14a8bf109e928f2a6
yousaf wazir <umbrellaman612@gmail.com>
2022-09-21 08:33:08 +0100

    feat: added home video

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,16 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <>Home</>
+    <section className="px-4 mx-auto max-w-screen-2xl md:h-[40rem] h-[30rem]">
+      <iframe
+        className="w-full h-full"
+        src="https://www.youtube-nocookie.com/embed/PZIqV7wNyyU?autoplay=1"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </section>
   )
 }
 
